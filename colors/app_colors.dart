9e85788794ff96f7c62ea8a8dd7690e161a11a66
Muhammad Ahmad Ansari <58669002+sudoahmed@users.noreply.github.com>
2024-09-18 00:40:15 +0500

    Create app_colors.dart

---
+++ colors/app_colors.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xff000000);
+  static const Color eerieBlack = Color(0xff181919);
+  static const Color blackFiveC = Color(0xff5C5C5C);
+  static Color boxColorShader = Colors.grey.withOpacity(0.07);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color gunMetal = Color(0xff28303F);
+  static const Color yellowOrange = Color(0xffFCB13E);
+  static const Color meatBrown = Color(0xffEDAF35);
+
+  static const Color chromeYellow = Color(0xffFFA800);
+  static const Color dimGray = Color(0xff6C6C6C);
+  static const Color vividCerulean = Color(0xff00AAD5);
+  static Color vividCeruleanLight = const Color.fromRGBO(0, 170, 213, 0.1);
+  static const Color brightGray = Color(0xffEEEEEE);
+  static const Color darkSilver = Color(0xff707070);
+  static const Color darkSilver6F = Color(0xff6F6F6F);
+  static const Color davySGrey = Color(0xff575757);
+  static const Color graniteGray63 = Color(0xff636363);
+  static const Color sonicSilver74 = Color(0xff747474);
+  static const Color errorRed = Color(0xffFF453A);
+  static const Color sonicSilver75 = Color(0xff757575);
+  static const Color sonicSilver78 = Color(0xff787878);
+  static const Color cultured = Color(0xffF6F6F6);
+  static const Color coralRed = Color(0xffFF3B30);
+  static const Color chineseBlack = Color(0xff0E1014);
+  static const Color graniteGray65 = Color(0xff646565);
+  static const Color ufoGreen69 = Color(0xff3CDC69);
+  static const Color ufoGreen59 = Color(0xff34C759);
+  static const Color graniteGray62 = Color(0xff5E5F62);
+  static const Color pictonBlue = Color(0xff35BCDE);
+  static const Color spanishGray = Color(0xff959595);
+  static const Color philippineGray = Color(0xff8F8F8F);
+  static const Color platinumGray = Color(0xffE3E3E3);
+  static const Color vividCeruleanligth = Color(0xff00ABF4);
+  static const Color vividCeruleanMid = Color(0xff1CADFF);
+
+  static const Color circularColor = Color(0xffFFC107);
+  static const Color yaleBlue = Color(0xff1A5091);
+  static const Color antiFlashWhite = Color(0xffF3F2F2);
+  static const Color offWhite = Color(0xffF5F5F5);
+
+  static const Color grayColor = Color(0xffDFDFDF);
+  static const Color americanSilver = Color(0xffCACED7);
+  static const Color darkGray = Color(0xffAAAAAA);
+  static const Color chineseSilver = Color(0xffCDCCCB);
+  static const Color graniteGray = Color(0xff666666);
+  static const Color oldGrey = Color(0xff7C7C7C); //🤣
+  static const Color lightGrey = Color(0xfff3f3f3); //🤣
+  static const Color old2Grey = Color(0xff808080); //🤣
+  static const Color lightGray = Color(0xffD2D2D2);
+  static const Color platinum = Color(0xffE8E8E8);
+}
